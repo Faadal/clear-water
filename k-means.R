3d44873a9152dbdfe4b0ba9b7441a608e9439bc9
Nick Lucius <nicholas.lucius2@cityofchicago.org>
2017-09-12 15:59:00 -0400

    only use last 5 years for kmeans

--- k-means.R
+++ k-means.R
@@ -9,6 +9,20 @@ reclusters <- 5
 source("R/00_Startup.R")
 df <- readRDS(paste0(getwd(),"/Data/df.Rds"))
 dt <- data.table(df)
+
+dt <- dt[Year %in% c(#"2006",
+                     #"2007",
+                     #"2008",
+                     #"2009",
+                     #"2010",
+                     #"2011",
+                     "2012",
+                     "2013",
+                     "2014",
+                     "2015",
+                     "2016",
+                     "2017")]
+
 dt[Escherichia.coli >= 235, exceedance := 1]
 dt[Escherichia.coli < 235, exceedance := 0]
 dt_byBeach <- dt[!is.na(exceedance),
@@ -30,18 +44,18 @@ dt_byBeach[order(exceedances, decreasing = TRUE)]
 
 # remove worst beaches (these are the beaches to always test)
 
-km2 <- kmeans(scale(dt_byBeach[exceedances < 165,
+km2 <- kmeans(scale(dt_byBeach[exceedances < 70,
                          2:3]),
              centers = reclusters,
              nstart = 100)
 
-plot(dt_byBeach[exceedances < 165,
+plot(dt_byBeach[exceedances < 70,
                 2:3],
      col =(km2$cluster +1),
      main=paste0("K-Means result with ", reclusters, " reclusters"), 
      pch=20, 
      cex=2)
-dt_byBeach[exceedances < 165,"recluster"] <- km2$cluster
+dt_byBeach[exceedances < 70,"recluster"] <- km2$cluster
 
 dt_byBeach[order(exceedances, decreasing = TRUE)]
 
