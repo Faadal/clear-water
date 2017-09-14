3b35348e2745e395574751060b267f9018032c45
Nick Lucius <nicholas.lucius2@cityofchicago.org>
2017-09-14 12:39:36 -0400

    add longitude and breakwater length

--- k-means.R
+++ k-means.R
@@ -27,13 +27,35 @@ dt[Escherichia.coli >= 235, exceedance := 1]
 dt[Escherichia.coli < 235, exceedance := 0]
 dt_byBeach <- dt[!is.na(exceedance),
                  .(exceedances = sum(exceedance)),
-                 .(Client.ID, Latitude)]
+                 .(Client.ID, Latitude, Longitude)]
+dt_byBeach$breakwater <- 0
+dt_byBeach[Client.ID == "12th"]$breakwater <- 108
+dt_byBeach[Client.ID == "31st"]$breakwater <- 415
+dt_byBeach[Client.ID == "39th"]$breakwater <- 128
+dt_byBeach[Client.ID == "57th"]$breakwater <- 442
+dt_byBeach[Client.ID == "63rd"]$breakwater <- 1215
+dt_byBeach[Client.ID == "Albion"]$breakwater <- 0
+dt_byBeach[Client.ID == "Calumet"]$breakwater <- 536
+dt_byBeach[Client.ID == "Foster"]$breakwater <- 202
+dt_byBeach[Client.ID == "Howard"]$breakwater <- 0
+dt_byBeach[Client.ID == "Juneway"]$breakwater <- 0
+dt_byBeach[Client.ID == "Leone"]$breakwater <- 41
+dt_byBeach[Client.ID == "Montrose"]$breakwater <- 977
+dt_byBeach[Client.ID == "North Avenue"]$breakwater <- 499
+dt_byBeach[Client.ID == "Oak Street"]$breakwater <- 0
+dt_byBeach[Client.ID == "Ohio"]$breakwater <- 1381
+dt_byBeach[Client.ID == "Osterman"]$breakwater <- 265
+dt_byBeach[Client.ID == "Rainbow"]$breakwater <- 1002
+dt_byBeach[Client.ID == "Rogers"]$breakwater <- 75
+dt_byBeach[Client.ID == "South Shore"]$breakwater <- 387
+dt_byBeach[Client.ID == "Jarvis"]$breakwater <- 0
+
 
 set.seed(317)
-km <- kmeans(scale(dt_byBeach[,2:3]),
+km <- kmeans(scale(dt_byBeach[,2:5]),
              centers = clusters,
              nstart = 100)
-plot(dt_byBeach[,2:3],
+plot(dt_byBeach[,3:2],
      col =(km$cluster +1),
      main=paste0("K-Means result with ", clusters, " clusters"), 
      pch=20, 
@@ -42,20 +64,20 @@ dt_byBeach$cluster <- km$cluster
 
 dt_byBeach[order(exceedances, decreasing = TRUE)]
 
-# remove worst beaches (these are the beaches to always test)
+# remove worst clusters (these are the beaches to always test)
 
-km2 <- kmeans(scale(dt_byBeach[exceedances < 70,
+km2 <- kmeans(scale(dt_byBeach[!cluster %in% c(1,4,5),
                          2:3]),
              centers = reclusters,
              nstart = 100)
 
-plot(dt_byBeach[exceedances < 70,
+plot(dt_byBeach[!cluster %in% c(1,4,5),
                 2:3],
      col =(km2$cluster +1),
      main=paste0("K-Means result with ", reclusters, " reclusters"), 
      pch=20, 
      cex=2)
-dt_byBeach[exceedances < 70,"recluster"] <- km2$cluster
+dt_byBeach[!cluster %in% c(1,4,5),"recluster"] <- km2$cluster
 
 dt_byBeach[order(exceedances, decreasing = TRUE)]
 
